142125efbd57e68a52988fd18d7ac9caffbd13e7
Atakan Him <52455771+atakanhim@users.noreply.github.com>
2024-02-15 01:07:03 +0300

    c

---
+++ Core/itApp.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itApp.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class
+    {
+        Task<bool> AddAsync(T entity);
+        Task<bool> AddRangeAsync(List<T> entitys);
+
+        bool Remove(T entity);
+        bool RemoveRange(List<T> entitys);
+        Task<bool> RemoveAsync(string id);
+
+        bool Update(T entity);
+
+        Task<int> SaveAsync();
+    }
+}
