55b3f688c1420d92b60284dc712ea37cbfe2e67d
Atakan Him <52455771+atakanhim@users.noreply.github.com>
2024-03-03 14:29:48 +0300

    checkmar controller

---
+++ Presentation/itApp.API/Controllers/CheckMarkController.cs
@@ -0,0 +1,27 @@
+﻿using itApp.Application.Features.Commands.CheckMark.CreateCheckMark;
+using itApp.Application.Features.Commands.Department.CreateDepartment;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace itApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckMarkController : ControllerBase
+    {
+        readonly IMediator _mediator;
+
+        public CheckMarkController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Create([FromBody] CreateCheckMarkCommandRequest request)
+        {
+            CreateCheckMarkCommandResponse response = await _mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}
